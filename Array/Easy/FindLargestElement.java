4d73c82adb27c488b58641077f2f7d865aeb77b5
Ishita777 <srivastavaishita810@gmail.com>
2026-05-01 21:52:42 +0530

    insertion and selection sort

---
+++ Array/Easy/FindLargestElement.java
@@ -0,0 +1,22 @@
+package Array.Easy;
+
+import Utilties.Utility;
+
+import java.util.Scanner;
+
+public class FindElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numToFind = sc.nextInt();
+
+        Utility utilObj = new Utility(sc);
+        utilObj.createIntList(numToFind);
+
+        
+
+    }
+
+}
